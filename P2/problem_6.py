1072c04b8ccbf9ed7b420f6c6eccd915605df05b
Joris Vial <joris.vial@hotmail.com>
2020-08-27 06:59:20 +0900

    Cleanup problem6 tests

--- P2/problem_6.py
+++ P2/problem_6.py
@@ -7,6 +7,9 @@ def get_min_max(ints):
     Args:
        ints(list): list of integers containing one or more integers
     """
+    if ints is None or len(ints) == 0:
+        return None
+
     min_value = sys.maxsize
     max_value = -sys.maxsize - 1
 
@@ -18,21 +21,37 @@ def get_min_max(ints):
 
     return min_value, max_value
 
-## Example Test Case of Ten Integers
+
+#########################################
+## Tests
+#########################################
+
 import random
 
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
-print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+print("get_min_max(0 => 10), Expected", (0, 9))
+assert (0, 9) == get_min_max(l)
 
 l = [i for i in range(-10, 10)]  # a list containing -10 - 9
 random.shuffle(l)
-print ("Pass" if ((-10, 9) == get_min_max(l)) else "Fail")
+print("get_min_max(-10 => 10), Expected", (-10, 9))
+assert (-10, 9) == get_min_max(l)
 
 l = [i for i in range(-123456789, -123456788)]  # a list containing -123456789
 random.shuffle(l)
-print ("Pass" if ((-123456789,-123456789) == get_min_max(l)) else "Fail")
+print("get_min_max(-123456789 => -123456788), Expected", (-123456789,-123456789))
+assert (-123456789,-123456789) == get_min_max(l)
 
 l = [i for i in range(0, 1)]  # a list containing 0
 random.shuffle(l)
-print ("Pass" if ((0, 0) == get_min_max(l)) else "Fail")
+print("get_min_max(0 => 1), Expected", (0, 0))
+assert (0, 0) == get_min_max(l)
+
+print("get_min_max([]), Expected", "None")
+assert None == get_min_max([])
+
+print("get_min_max(None), Expected", "None")
+assert None == get_min_max(None)
+
+print("All tests passed")
